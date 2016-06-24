54ae50dd88a201e692501cb844a186e51218f3d7
Troy Parkinson <Troy Parkinson>
2016-06-23 20:59:45 -0700

    added file logging stream

--- src/util/logging.js
+++ src/util/logging.js
@@ -1,16 +1,15 @@
 "use strict";
 
-const _ = require("lodash");
+const fs = require("fs");
 const path = require("path");
 const proc = require("process");
 const bunyan = require("bunyan");
+const _ = require("lodash");
 
 const CONFIG_PATH = "../../bunyan.json";
 let config = require(CONFIG_PATH);
 
 const env = proc.env.NODE_ENV || "development";
-console.log("env: " + env);
-
 if (env === "development" && config.development) {
     _.merge(config, config.development);
 }
@@ -21,16 +20,21 @@ if (env === "production" && config.production) {
 
 delete config.development;
 delete config.production;
-console.log(config);
 
-var conf = _.assign(config, {
+let conf = _.assign(config, {
     name: 'ping-pong',
     serializers: {
         err: bunyan.stdSerializers.err
     }
 });
 
-var log = bunyan.createLogger(conf);
-log.debug('logging configured...');
+if (conf.streams && _.some(conf.streams, (x) => x.path)) {
+    if (!fs.existsSync("./logs")) {
+        fs.mkdirSync("./logs");
+    }
+}
+
+let log = bunyan.createLogger(conf);
+log.debug('logging configured!');
 
 module.exports = log;
